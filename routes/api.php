e5ae955217ae3544815d0a350d2552c502b0145c
JorikDura <vandarkslave@outlook.com>
2024-08-28 15:52:59 +0300

    Init

---
+++ routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    //nothing here right now
+});
---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,7 @@
+<?php
+
+use function PHPUnit\Framework\assertTrue;
+
+it('example test', function () {
+    assertTrue(true);
+});
